f255c67b6073cd58f5583e100ea82662183df295
vattd <sulucus@gmail.com>
2020-11-29 20:42:21 +0100

    added idempotent operations for cass

--- cass/merge.go
+++ cass/merge.go
@@ -1,10 +1,17 @@
 package cass
 
+import "strings"
+
 type MergeTableCtx struct {
 	RemoteTables map[string]*Table
 	LocalTables  map[string]*Table
 }
 
+func MergeAddOperation(dest *string, elem string) {
+	if !strings.Contains(*dest, elem) {
+		*dest += elem
+	}
+}
 func MergeTableCtxNew() *MergeTableCtx {
 	ret := new(MergeTableCtx)
 	ret.LocalTables = make(map[string]*Table)
@@ -57,16 +64,17 @@ func MergeColumns(
 	stmt *StmtCtx,
 	sctx *MergeScriptCtx,
 	lt, rt *Table,
+	tctx *MergeTableCtx,
 ) {
 	// iterate on local columns
 	for lc := range lt.Columns {
 		if rc, ok := rt.Columns[lc]; ok {
 			// if found matching column then alter
 			// (returns empty string if columns are the same)
-			*sctx.Create += stmt.AlterColumn(lt.Name, lt.Columns[lc], rc)
+			MergeAddOperation(sctx.Create, stmt.AlterColumn(lt.Name, lt.Columns[lc], rc))
 		} else {
 			// if you didnt find matching column on remote then create it
-			*sctx.Create += stmt.AddColumn(lt.Name, lt.Columns[lc])
+			MergeAddOperation(sctx.Create, stmt.AddColumn(lt.Name, lt.Columns[lc]))
 		}
 	}
 
@@ -75,7 +83,10 @@ func MergeColumns(
 		if _, ok := lt.Columns[rc]; !ok {
 			// if local schema doesnt contain remote column
 			// then drop it from database
-			*sctx.Drop += stmt.DropColumn(lt.Name, rt.Columns[rc])
+			MergeAddOperation(sctx.Drop, stmt.DropColumn(lt.Name, rt.Columns[rc]))
+			for ixk := range lt.SASIIndexes {
+				MergeAddOperation(sctx.Create, stmt.DropIndex(lt.SASIIndexes[ixk].Name))
+			}
 		}
 	}
 }
@@ -95,16 +106,16 @@ func MergeSASIIndexes(
 		if rix, ok := rt.SASIIndexes[lixName]; ok {
 			if !MergeCmpSASIIndex(lix, rix) {
 				// if indexes differ we will recreate them
-				*sctx.Drop += stmt.DropIndex(lix.Name)
-				*sctx.Create += stmt.CreateSASIIndex(rt.Name, rix)
+				MergeAddOperation(sctx.Drop, stmt.DropIndex(lix.Name))
+				MergeAddOperation(sctx.Create, stmt.CreateSASIIndex(rt.Name, rix))
 			}
 		} else {
-			*sctx.Create += stmt.CreateSASIIndex(lt.Name, lix)
+			MergeAddOperation(sctx.Create, stmt.CreateSASIIndex(lt.Name, lix))
 		}
 	}
 	for rixName := range rt.SASIIndexes {
 		if _, ok := lt.SASIIndexes[rixName]; !ok {
-			*sctx.Drop += stmt.DropIndex(rixName)
+			MergeAddOperation(sctx.Drop, stmt.DropIndex(rixName))
 		}
 	}
 }
@@ -122,25 +133,25 @@ func MergeTables(
 			// if primary key differs
 			// then we dont have other choice than to recreate the table
 			if MergeCmpPK(lt.PrimaryKey, rt.PrimaryKey) {
-				MergeColumns(stmt, sctx, lt, rt)
+				MergeColumns(stmt, sctx, lt, rt, tctx)
 				MergeSASIIndexes(stmt, sctx, lt, rt)
 			} else {
 				for ixk := range lt.SASIIndexes {
-					*sctx.Drop += stmt.DropIndex(lt.SASIIndexes[ixk].Name)
+					MergeAddOperation(sctx.Drop, stmt.DropIndex(lt.SASIIndexes[ixk].Name))
 				}
-				*sctx.Drop += stmt.DropTable(lt)
-				*sctx.Create += stmt.CreateTable(rt)
+				MergeAddOperation(sctx.Drop, stmt.DropTable(lt))
+				MergeAddOperation(sctx.Create, stmt.CreateTable(rt))
 				for ixk := range rt.SASIIndexes {
 					ix := rt.SASIIndexes[ixk]
-					*sctx.Create += stmt.CreateSASIIndex(k, ix)
+					MergeAddOperation(sctx.Create, stmt.CreateSASIIndex(k, ix))
 				}
 			}
 		} else {
 			// remote table doesnt exist
-			*sctx.Create += stmt.CreateTable(lt)
+			MergeAddOperation(sctx.Create, stmt.CreateTable(lt))
 			for ixk := range lt.SASIIndexes {
 				ix := lt.SASIIndexes[ixk]
-				*sctx.Create += stmt.CreateSASIIndex(k, ix)
+				MergeAddOperation(sctx.Create, stmt.CreateSASIIndex(k, ix))
 			}
 		}
 	}
@@ -155,18 +166,18 @@ func MergeViews(
 		lv := vctx.LocalViews[k]
 		if rv, ok := vctx.RemoteViews[k]; ok {
 			if !MergeCmpPK(lv.PrimaryKey, rv.PrimaryKey) {
-				*sctx.Drop += stmt.DropMaterializedView(lv)
-				*sctx.Create += stmt.CreateMaterializedView(rv)
+				MergeAddOperation(sctx.Drop, stmt.DropMaterializedView(lv))
+				MergeAddOperation(sctx.Create, stmt.CreateMaterializedView(rv))
 			}
 		} else {
 			// remote table doesnt exist
-			*sctx.Create += stmt.CreateMaterializedView(lv)
+			MergeAddOperation(sctx.Create, stmt.CreateMaterializedView(lv))
 		}
 	}
 	for k := range vctx.RemoteViews {
 		rv := vctx.RemoteViews[k]
 		if _, ok := vctx.LocalViews[k]; !ok {
-			*sctx.Drop += stmt.DropMaterializedView(rv)
+			MergeAddOperation(sctx.Drop, stmt.DropMaterializedView(rv))
 		}
 	}
 }
